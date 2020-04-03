c6f7b6f2c139b3a867af92c60fa2a4f87b365c17
Anish <anishanne1@gmail.com>
2020-04-03 18:40:17 -0400

    Y

--- statcord/exceptions.py
+++
@@ -1,8 +0,0 @@
-class StatcordException(Exception):
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
-
-
-class RequestFailure(StatcordException):
-    def __init__(self, status: int, response: str):
-        super().__init__("{}: {}".format(status, response))
